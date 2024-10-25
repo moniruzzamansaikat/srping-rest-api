c6b7b79d19b4b88f5e77299dacfddaa41cbb0488
moniruzzamansaikat <saikat.moniruzzaman@gmail.com>
2024-10-25 16:54:06 +0600

    useless bean

--- src/main/java/saikat/test/restapi/users/UserService.java
+++ src/main/java/saikat/test/restapi/users/UserService.java
@@ -3,7 +3,6 @@ package saikat.test.restapi.users;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,7 +10,6 @@ public class UserService {
 
     private final UserRepository userRepository;
 
-    @Autowired
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
